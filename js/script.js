2be09cad384735275b989650d054efefc5ead69f
FrancescoAceto <acetof02@gmail.com>
2023-02-22 18:15:40 +0100

    Fine esercizio

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/* Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina la concatenazione delle variabili + un numero a caso scelto da voi
+*/
+
+let Username;
+
+Username = prompt("Qual è il tuo nome?");
+
+let Surname;
+
+Surname = prompt("Qual è il tuo cognome?");
+
+let Favcolor;
+
+Favcolor = prompt("Qual è il tuo colore preferito?");
+
+document.writeln(Username + Surname + Favcolor + 3);
